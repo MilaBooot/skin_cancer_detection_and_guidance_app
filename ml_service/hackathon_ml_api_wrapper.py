4a403fb84b7c839f4e5206a31e16995b993180d0
Krishnan <akrishnan394@gmail.com>
2020-09-26 22:03:51 +0530

    Update hackathon_ml_api_wrapper.py

--- ml_service/hackathon_ml_api_wrapper.py
+++ ml_service/hackathon_ml_api_wrapper.py
@@ -19,25 +19,21 @@ IMAGE_SIZE = 256
 IMAGE_DIMENSION = 3
 
 class hackathon_ml_api_wrapper:
-
+  
+    #Function Description: This function takes user image as input and resizes it as required
+    #Function Input: uUer image
+    #Function Output: Resized image
     def image_resize (self, bff_input, IMAGE_SIZE, IMAGE_DIMENSION):
-
-        image_array = cv2.imread(bff_input)
-        image = cv2.resize(image_array, (IMAGE_SIZE, IMAGE_SIZE, IMAGE_DIMENSION))
+        
+        image = cv2.resize(-1, image_array, (IMAGE_SIZE, IMAGE_SIZE, IMAGE_DIMENSION))
         return image
-
-
-    def predict_model (self, image):
-        print('Loading the model')
-
-        #Load the model from db/json file
-        #return the predicted value
-        return y_predict
-
+      
+    #Function Description: This function identifies the risk label based on y_predict percentage and threshold value
+    #Function Input: y_predict, threshold and risk label
+    #Function Output: risk label
     def compute_risk_using_image (self, y_predict, THRESHOLD, RISK_LABEL):
-        print ('Computing risk')
 
-        if(y_predict[0]>THRESHOLD[0]):
+      if(y_predict[0]>THRESHOLD[0]):
             return RISK_LABEL[0]
         elif (y_predict[0]>THRESHOLD[1]) & (y_predict[0]<THRESHOLD[0]):
             return RISK_LABEL[1]
@@ -45,7 +41,10 @@ class hackathon_ml_api_wrapper:
             return RISK_LABEL[2]
         else:
             return RISK_LABEL[3]
-
+      
+    #Function Description: This function identifies the risk using questionarie
+    #Function Input: questionarie
+    #Function Output:  weights
     def compute_weight_using_questionarie(self, answer):
 
         #Local Variable Definition
@@ -72,28 +71,40 @@ class hackathon_ml_api_wrapper:
 
         return weight
 
+            
+    #Function Description: This is the decision logic
+    #Function Input: weight, y_predict, RISK LABEL
+    #Function Output: list which contains 4 values
     def decision_logic(self, weight, y_predict, RISK_LABEL):
+        
+        if RISK_LABEL=='NOT_APPLICABLE':
+            temp_cancer = 'NO'
+        else:
+            temp_cancer = 'YES'
+          
         o_probability = weight * y_predict[0]
-
-        o_result = [math.ceil(o_probability), RISK_LABEL, y_predict[1]]
-
+    
+        o_result = [temp_cancer, math.ceil(o_probability), RISK_LABEL, y_predict[1]]
 
         return o_result
 
+print ('creating class handles')
+HMLAPIW = hackathon_ml_api_wrapper()
 
 
-#call this function for ml_api
-def predict_cancer(bff_input, answer, THRESHOLD, RISK_LABEL, IMAGE_SIZE, IMAGE_DIMENSION):
-    print ('predicting...')
-    print ('creating class handles')
-    HMLAPIW = hackathon_ml_api_wrapper()
-    
-    print ('Resizing input image')
+#Function Description: This function is called by ml_api. It sequences akk the above function calls
+#Function Input: weight, y_predict, RISK LABEL
+#Function Output: list which contains 4 values
+def predict_cancer(bff_input, answer):
+  
+    print ('Input image is read and resizing is in progress')
     image = HMLAPIW.image_resize(bff_input=bff_input, IMAGE_SIZE=IMAGE_SIZE, IMAGE_DIMENSION=IMAGE_DIMENSION)
     y_predict = HMLAPIW.predict_model(image=image)
     risk = HMLAPIW.compute_risk_using_image(y_predict=y_predict, THRESHOLD=THRESHOLD, RISK_LABEL=RISK_LABEL)
     weight = HMLAPIW.compute_weight_using_questionarie(answer=answer)
-    o_result = HMLAPIW.decision_logic(weight=weight, y_predict=y_predict, RISK_LABEL=risk)
+    o_result = HMLAPIW.decision_logic(weight=weight, y_predict=y_predict, RISK_LABEL=risk, )
+    
+    return o_result
 
 
 
