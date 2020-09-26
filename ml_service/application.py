818b7aeccf7df759908accc763f815d1ff3cd8f5
deepak7946 <deepak7946@gmail.com>
2020-09-27 00:11:49 +0530

    file name changes for AWS and calling predict in api function

--- ml_service/application.py
+++ ml_service/application.py
@@ -9,8 +9,9 @@ from PIL import Image
 import io
 import numpy as np
 import cv2
+from hackathon_ml_api_wrapper import predict_cancer
 
-flask_app = Flask(__name__)
+application = flask_app = Flask(__name__)
 app = Api(app = flask_app)
 
 ml_api = app.namespace('mlService', description='ML service API')
@@ -32,7 +33,7 @@ class dataFields:
 	def predict_data(self):
 		reponse_dict = ml_api.model("reponse_dict", {"id":fields.String, "answer":fields.String})
 		resource_fields = ml_api.model("ML predict data",
-		   {'image': fields.List(fields.Integer(description="image", required=True)),
+		   {'image': fields.String(description="image", required=True),
 			'questions': fields.List(fields.Nested(reponse_dict, required=True))})
 		return resource_fields
 
@@ -41,22 +42,24 @@ class dataFields:
 class login(Resource):
 	@ml_api.expect(dataFields().predict_data())
 	@ml_api.response(200, '{result:{"data":{"cancer":"yes/no", "value":proablity value (float), \
-	"type": "type of cancer predicted"}}}')
+	"type": "type of cancer predicted", "Risk Factor": (Low/Medium/High)}}}')
 	@ml_api.response(401, 'User ID not found')
 	def post(self):
 		json_data = request.json
 		image = json_data["image"]
 		response = json_data["questions"]
 		image_data = self.convert_imgdata(image)
-		cv2.imwrite("out.jpg", image_data)
-		data = {"cancer":"yes", "value":0.75, "type": "Basal cell carsinoma"}
-		#try:
-	#		data = ldb.get_user_details(user_id)
-#		except KeyError:#
-			#abort(401, result=msgFormats().error_msg("User ID not found"))
+		try:
+			data = predict_cancer(image_data, response)
+		except KeyError:
+			abort(401, result=msgFormats().error_msg("User ID not found"))
 		return msgFormats().data_msg(data)
 
 	def convert_imgdata(self, img_str):
 		imgdata = base64.b64decode(img_str)
 		image = Image.open(io.BytesIO(imgdata))
 		return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+
+if __name__ == "__main__":
+        application.run(port=5001, debug=True)
