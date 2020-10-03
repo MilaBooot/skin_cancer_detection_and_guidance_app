2968bee2f9b129a794e825078f126a5fc4efd0f9
shandhanu <shandhanu@qburst.com>
2020-10-03 13:46:39 +0530

    added functionality to list user records

--- bff-node/src/main/java/com/skincancerdetection/bffnode/router/CommonServiceRouter.java
+++ bff-node/src/main/java/com/skincancerdetection/bffnode/router/CommonServiceRouter.java
@@ -1,13 +1,14 @@
 package com.skincancerdetection.bffnode.router;
 
-import com.skincancerdetection.bffnode.model.CommonResponse;
-import com.skincancerdetection.bffnode.model.DoctorEnquiryRequest;
-import com.skincancerdetection.bffnode.model.UserDetailsDto;
-import com.skincancerdetection.bffnode.model.UserInfoRequestDto;
+import com.skincancerdetection.bffnode.model.*;
 
 public interface CommonServiceRouter {
     CommonResponse registerUser(UserDetailsDto userDetailsDto);
     CommonResponse retrieveUser(UserInfoRequestDto userInfoRequestDto);
     CommonResponse getQuestionnaire();
     CommonResponse getDoctors(double longitude, double latitude);
+    CommonResponse uploadDocument(FileUploadDto fileUploadDto);
+    CommonResponse getUserDocuments(String username);
+    CommonResponse getFile(String username, String filename);
+    void deleteDocument(String username, String filename);
 }
